3b8be4b6d07b60ec025754ea6b43c735926e989e
ktht <karlehataht@gmail.com>
2021-11-07 22:10:35 +0200

    Script for setting up env needed by DelphesNtuplizer

---
+++ set_env.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# source this file
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+case :$LD_LIBRARY_PATH: in
+  *:$SCRIPT_DIR:*)  ;;  # do nothing
+  *) LD_LIBRARY_PATH=$SCRIPT_DIR:$LD_LIBRARY_PATH ;;
+esac
+
+case :$ROOT_INCLUDE_PATH: in
+  *:$SCRIPT_DIR/external:*)  ;;  # do nothing
+  *) ROOT_INCLUDE_PATH=$SCRIPT_DIR/external:$ROOT_INCLUDE_PATH ;;
+esac
